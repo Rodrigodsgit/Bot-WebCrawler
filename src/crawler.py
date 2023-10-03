6029ce379335ae6cf72d9f24691e221575974f59
Rodrigo Damasceno <damasceno.s.rodrigo@gmail.com>
2023-10-03 18:38:21 -0300

    Add: Extração da pagina InfoMoney

--- src/crawler.py
+++ src/crawler.py
@@ -3,21 +3,39 @@ from bs4 import BeautifulSoup
 
 from links import LINK_1, LINK_2
 
-
 def extractData(url):
     response = requests.get(url)
-
     soup = BeautifulSoup(response.content, 'html.parser')
-
     return soup
 
 def crawlerInfoMoney():
     soup = extractData(LINK_1)
 
-    positiveActions, negativeActions = soup.find_all("table",{"class": "default-table"})
+    table = soup.find("table", {"class": "default-table"})
 
-    return positiveActions, negativeActions
+    rows = table.find_all("tr")
+
+    data = []
+
+    for row in rows:
+        # Encontrar todas as tags <a> dentro da linha
+        a_tags = row.find_all("a")
+        
+        # Iterar sobre as tags <a> e extrair o atributo 'href'
+        href_values = [a['href'] for a in a_tags]
+
+        # Extrair o texto das células restantes
+        cell_texts = [cell.text.strip() for cell in row.find_all("td")]
+
+        # Adicionar os valores ao data
+        row_data = href_values + cell_texts
+        data.append(row_data)
+
+    print(data)
+    return data
+def crawlerValorInvest():
+    soup = extractData(LINK_2)
+    
+    
 
 teste = crawlerInfoMoney()
-print(teste)
-
